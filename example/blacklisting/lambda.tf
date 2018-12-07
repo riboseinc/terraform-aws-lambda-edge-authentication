c59ad206639e5001cea3d6f098ab46b2ed244cd2
Phuong H <phuonghqh@gmail.com>
2018-12-07 20:38:56 +0700

    Blacklisting sample

---
+++ example/blacklisting/lambda.tf
@@ -0,0 +1,15 @@
+locals {
+  bucket_name = "phuonghqh.booppi.website.htaccess"
+  bucket_key = "htaccess.json"
+  basic_user = "test"
+  basic_password = "test"
+}
+
+module "lambda" {
+  source = "../../"
+
+  bucket_name    = "${local.bucket_name}"
+  bucket_key     = "${local.bucket_key}"
+  basic_user     = "${local.basic_user}"
+  basic_password = "${local.basic_password}"
+}
