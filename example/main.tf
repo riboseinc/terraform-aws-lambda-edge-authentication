b1dccb55a127b60fc57a34efcb5b28d0060b39c9
Phuong H <phuonghqh@gmail.com>
2018-12-19 13:41:55 +0700

    Update sample whitelist & blacklist

---
+++ example/main.tf
@@ -0,0 +1,34 @@
+module "lambda" {
+  source = "../"
+
+  bucket_name    = "${var.s3_access_name}"
+  bucket_key     = "${var.s3_access_key}"
+  basic_user     = "${var.lambda_basic_username}"
+  basic_password = "${var.lambda_basic_password}"
+}
+
+
+module "main" {
+//  source = "https://github.com/riboseinc/terraform-aws-s3-cloudfront-website"
+  source = "/Users/capitant/working/terraform-aws-s3-cloudfront-website"
+
+  fqdn = "${var.fqdn}"
+  ssl_certificate_arn = "${aws_acm_certificate_validation.cert.certificate_arn}"
+  allowed_ips = "${var.allowed_ips}"
+
+  index_document = "index.html"
+  error_document = "404.html"
+
+  refer_secret = "${base64sha512("REFER-SECRET-19265125-${var.fqdn}-52865926")}"
+
+  force_destroy = "true"
+
+  providers {
+    "aws.main" = "aws.main"
+    "aws.cloudfront" = "aws.cloudfront"
+  }
+
+  lambda_edge_enabled = "true"
+  lambda_edge_event_type = "viewer-request"
+  lambda_edge_arn_version = "${module.lambda.arn}:${module.lambda.version}"
+}
