5f8df59be67d099abfea623c3919e460463ff980
Phuong H <phuonghqh@gmail.com>
2018-12-20 00:10:52 +0700

    Update example to multiple tf files

--- example/s3_access.tf
+++ example/s3_access.tf
@@ -2,5 +2,5 @@ resource "aws_s3_bucket_object" "object" {
   bucket = "${var.s3_access_name}"
   key    = "${var.s3_access_key}"
   source = "${path.module}/htaccess.json"
-  etag   = "${md5(file("path/to/file"))}"
+  etag   = "${md5(file("${path.module}/htaccess.json"))}"
 }
