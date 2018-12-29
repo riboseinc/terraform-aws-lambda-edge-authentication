9aa9f46ee3a9fb6b1f8e23548e6eb67b54d3411b
Phuong H <phuonghqh@gmail.com>
2018-12-29 21:59:27 +0700

    Apply Htpasswd Auth for user config

--- src/basic_auth.ts
+++ src/basic_auth.ts
@@ -14,8 +14,6 @@ export class BasicAuth {
     private bucket_key: string;
 
     constructor() {
-        console.log('calling 123');
-
         const path = resolve("params.json");
         let text = readFileSync(path).toString();
         const params = JSON.parse(text);
@@ -23,24 +21,12 @@ export class BasicAuth {
     }
 
     async handler(event, context, callback) {
-        console.log(JSON.stringify(event));
         const request = event.Records[0].cf.request;
-        console.log(JSON.stringify(request));
         const uri = request.uri;
 
         try {
             const configRaw = await Utils.s3Read(this.bucket_name, this.bucket_key); //await this.readConfig();
-            console.log(`configRaw= ${configRaw}`);
             const config = new Config(configRaw);
-            // const filesStr = await this.readConfig();
-            // if (!filesStr || filesStr.trim().length == 0) {
-            //     throw new Error(`empty protect files => ignore`);
-            // }
-
-            // const uriPatterns = JSON.parse(filesStr);
-            // if (!Array.isArray(uriPatterns)) {
-            //     throw new Error('Bucket key is not a Json Array => ignore');
-            // }
 
             const uriPatterns = config.uriPatterns;
             let uriProtected = false;
@@ -63,25 +49,6 @@ export class BasicAuth {
             }
 
             return this.authorized(config, request, callback);
-
-            // const authString = 'Basic ' + new Buffer(this.basic_user + ':' + this.basic_pwd).toString('base64');
-            // if (typeof headers.authorization === 'undefined' || headers.authorization[0].value !== authString) {
-            //     const body = 'Unauthorized';
-            //     const response = {
-            //         status: '401',
-            //         statusDescription: 'Unauthorized',
-            //         body: body,
-            //         headers: {
-            //             'www-authenticate': [{key: 'WWW-Authenticate', value: 'Basic'}]
-            //         },
-            //     };
-            //     return callback(null, response);
-            // }
-
-            /*
-                        htpasswd.authenticate(body.username, body.password, config.htpasswd).then((authenticated) => {
-                    if (authenticated) {
-            */
         }
         catch (e) {
             console.error(e);
@@ -113,15 +80,15 @@ export class BasicAuth {
     }
 
     private authorized(config: Config, request: any, callback: any) {
-        const expires = config.generateExpires();
-        const hash = config.generateSecurePathHash(expires, request.origin.custom.path + request.uri);
-        // const signature = `md5=` + config.generateSecurePathHash(expires, request.origin.custom.path + request.uri) + "&expires=" + expires;
-        const signature = `md5=${hash}&expires=expires`;
-        if (request.querystring) {
-            request.querystring = request.querystring + "&" + signature;
-        } else {
-            request.querystring = signature;
-        }
+        // const expires = config.generateExpires();
+        // const hash = config.generateSecurePathHash(expires, request.origin.custom.path + request.uri);
+        // // const signature = `md5=` + config.generateSecurePathHash(expires, request.origin.custom.path + request.uri) + "&expires=" + expires;
+        // const signature = `md5=${hash}&expires=expires`;
+        // if (request.querystring) {
+        //     request.querystring = request.querystring + "&" + signature;
+        // } else {
+        //     request.querystring = signature;
+        // }
         return callback(null, request);
     }
 }
